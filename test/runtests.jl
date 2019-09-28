f75bbecb8954731e38423ac0950389c7e833a21b
Roger Herikstad <roger.herikstad@gmail.com>
2019-09-28 19:27:35 +0800

    Adds tests for full entropy

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,3 +10,23 @@ using Test
 	te = TSR.tr_entropy(X)
 	@test te ≈ 0.3250829733914482
 end
+
+@testset "Full entropy" begin
+	idx1 = fill(3,20)
+	idx2 = [3, 3, 3, 4, 4, 3, 3, 3, 3, 3, 3, 4, 4, 3, 4, 3, 4, 3, 3, 3]
+	idx3 = [3, 3, 4, 2, 2, 3, 4, 2, 4, 2, 3, 3, 4, 4, 4, 4, 3, 3, 4, 3]
+	idx4 = [2, 3, 1, 3, 5, 5, 4, 2, 2, 1, 1, 2, 2, 1, 1, 3, 4, 3, 2, 4]
+	X = falses(5,20)
+	ee = fill(0.0, 4)
+	for (kk,idx) in enumerate([idx1,idx2,idx3,idx4])
+		fill!(X, false)
+		for (ii,_idx) in enumerate(idx)
+			X[_idx,ii] = true
+		end
+		ee[kk] = TSR.entropy(X)
+	end
+	@test ee[1] ≈ -0.0
+	@test ee[2] ≈ 0.6108643020548935
+	@test ee[3] ≈ 1.0549201679861442
+	@test ee[4] ≈ 1.5444795210968603
+end
