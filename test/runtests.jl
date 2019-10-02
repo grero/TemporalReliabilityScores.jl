ab8234b38c0f850a278cca168388103f6b5870e3
Roger Herikstad <roger.herikstad@gmail.com>
2019-10-02 14:06:28 +0800

    Fixes tests for temporal entropy

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,6 +20,7 @@ end
 end
 
 @testset "Full entropy" begin
+	RNG = MersenneTwister(1234)
 	idx1 = fill(3,20)
 	idx2 = [3, 3, 3, 4, 4, 3, 3, 3, 3, 3, 3, 4, 4, 3, 4, 3, 4, 3, 3, 3]
 	idx3 = [3, 3, 4, 2, 2, 3, 4, 2, 4, 2, 3, 3, 4, 4, 4, 4, 3, 3, 4, 3]
@@ -27,22 +28,27 @@ end
 	X = falses(5,20)
 	ee = fill(0.0, 4)
 	eer = fill(0.0, 4)
+	score = fill(0.0, 4)
 	for (kk,idx) in enumerate([idx1,idx2,idx3,idx4])
 		fill!(X, false)
 		for (ii,_idx) in enumerate(idx)
 			X[_idx,ii] = true
 		end
 		ee[kk] = TSR.entropy(X)
-		eer[kk] = TSR.renyientropy(X,2.0)
+		eer[kk],score[kk] = TSR.tr_entropy_score(X,2.0,RNG=RNG)
 	end
 	@test ee[1] ≈ -0.0
 	@test eer[1] ≈ -0.0
+	@test score[1] ≈ 0.0
 	@test ee[2] ≈ 0.6108643020548935
 	@test eer[2] ≈ 0.5447271754416722
+	@test score[2] ≈ 0.0
 	@test ee[3] ≈ 1.0549201679861442
 	@test eer[3] ≈ 1.021651247531981
+	@test score[3] ≈ 4.0
 	@test ee[4] ≈ 1.5444795210968603
 	@test eer[4] ≈ 1.491654876777717
+	@test score[4] ≈ 715.0
 end
 
 @testset "Temporal entropy score" begin
